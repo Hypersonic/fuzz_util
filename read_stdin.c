12db6f049eaedad09b4833e7b1cdd7bb35afa093
Josh Hofing <joshhofing@gmail.com>
2015-10-30 16:22:10 -0400

    Add read_stdin.c

---
+++ read_stdin.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char *read_stdin()
+{
+    size_t len = 1024, l = 0;
+    char *contents = malloc(len);
+    int c;
+    while ((c = getc(stdin)) != EOF) {
+        contents[l++] = c;
+        if (l == len) {
+            if (NULL == realloc(contents, len*2)) {
+                return NULL;
+            }
+            len *= 2;
+        }
+    }
+    contents[l] = '\0';
+    return contents;
+}
