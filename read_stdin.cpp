dae199a9e3bc4feabf5196d6e5b76fdd2a8b5462
Josh Hofing <joshhofing@gmail.com>
2015-10-30 16:23:01 -0400

    Add read_stdin.cpp

---
+++ read_stdin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+char *read_stdin()
+{
+    std::string contents = "";
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        contents += line + "\n";
+    }
+    return contents.c_str();
+}
+
+int main()
+{
+    std::string input = read_stdin();
+    std::cout << input << std::endl;
+    return 0;
+}
