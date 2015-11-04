c8c90eeab9531fea233724f8bbd0f1cfeb54a6d2
Jacob Edelman <edelman.jd@gmail.com>
2015-11-04 16:37:07 -0500

    Update find_errors.fish

--- find_errors.fish
+++ find_errors.fish
@@ -1,8 +1,12 @@
 #!/usr/bin/fish
 
-# fish find_errors.fish [output] [command that takes input file name as arg]
+# How to:
+# fish find_errors.fish [output] [string of a command to run the program, using @@ for the file name]
 # should be run in a directory that contains a crashes directory somewhere inside it (or recrusively buried inside it)
 # logs errors to [output], also writes unique errors to stdout as well as appending them to the output file
+
+# Example:
+# ./errors.fish out.txt "~/my_program @@"
 
 echo "" > $argv[1];
 for i in **/crashes/id*;
