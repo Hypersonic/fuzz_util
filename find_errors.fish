aec73483c99056fcaed673b7195a8c01a06da681
Jacob Edelman <edelman.jd@gmail.com>
2015-11-04 16:11:19 -0500

    Update find_errors.fish

--- find_errors.fish
+++ find_errors.fish
@@ -1,13 +1,12 @@
 #!/usr/bin/fish
 
-# fish find_errors.fish [command that takes input file name as arg]
+# fish find_errors.fish [output] [command that takes input file name as arg]
 # should be run in a directory that contains a crashes directory somewhere inside it (or recrusively buried inside it)
-# logs output to out.txt, also writes unique errors to stdout as well as appending them to out.txt
+# logs errors to [output], also writes unique errors to stdout as well as appending them to the output file
 
-rm out.txt;
-touch out.txt;
+echo "" > $argv[1];
 for i in **/crashes/id*;
-  echo -e "backtrace\nquit"               \
-  | gdb --quiet -ex "run" --args $argv $i \
-  | tee -a out.txt;
-end | grep --text "^(gdb) #0" | sort | uniq | tee -a out.txt
+  echo -e "backtrace\nquit"                                 \
+  | gdb --quiet -ex "run" --args $argv[2..(count $argv)] $i \
+  | tee -a $argv[1];
+end | grep --text "^(gdb) #0" | sort | uniq | tee -a $argv[1]
