3d2383b27c732c4ebe211b67a77cb2d9e9c28938
Josh Hofing <joshhofing@gmail.com>
2015-11-12 02:55:42 -0500

    Make clear_duplicates take arguments

--- clear_duplicates.py
+++ clear_duplicates.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
 from glob import glob
+from sys import argv, exit
 import os
 
+if len(argv) < 3:
+    print("Usage: python %s indir outdir"%argv[0])
+    exit(0)
+
+indir = argv[1]
+outdir = argv[2]
+
 files = []
-for fname in glob('fixed/*'):
+for fname in glob('%s/*'%indir):
     with open(fname) as f:
         files.append(f.read())
 
-os.mkdir('uniques')
+os.mkdir(outdir)
 files = list(set(files))
 for id,content in enumerate(files):
-    with open('uniques/%08d'%id, 'w') as f:
+    with open('%s/%08d'%(outdir, id), 'w') as f:
         f.write(content)
